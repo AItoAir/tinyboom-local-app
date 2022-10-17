471feb391f90fa137fc6da42e5acd15d2cad3909
Rowell <dev.rowellpica@gmail.com>
2022-10-17 12:30:03 +0800

    updated dockers/docker.sh script

--- dockers/run-docker.sh
+++ dockers/run-docker.sh
@@ -2,8 +2,8 @@
 
 docker run --rm \
        -p 127.0.0.1:1338:1337/tcp \
-       -v /Users/rowellpica/Workarea/jeong/tinyboom-local-app:/workarea/tinyboom-local-app \
-       -v /Users/rowellpica/Workarea/jeong/tmbs:/workarea/tmbs -i \
+       -v $(pwd)/..:/workarea/tinyboom-local-app \
+       -v $(pwd)/../tmbs:/workarea/tmbs -i \
       --name tinyboom-local \
       --entrypoint bash \
       -t tinyboom/localapp
